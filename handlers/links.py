1ab0694556a793a6fe2849781bb23abba3d38702
Brandon <esolutions0510@gmail.com>
2025-08-14 13:55:29 -0500

    removed unused folders

--- handlers/links.py
+++
@@ -1,9 +0,0 @@
-import re
-from utils.regex_patterns import URL_PATTERN, X_TWITTER_PATTERN
-
-def contains_non_x_links(text: str) -> bool:
-    urls = URL_PATTERN.findall(text)
-    for url in urls:
-        if not X_TWITTER_PATTERN.match(url):
-            return True
-    return False
